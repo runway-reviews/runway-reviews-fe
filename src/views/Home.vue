b65a318834475db109a445421ab4c41e2aba54e5
Zen McMillan <zenmcmillan@gmail.com>
2024-02-03 19:16:12 -0600

    Fix routing to route to AirportDetails page when airport is selected from AirportDropdown component

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <AirportHeader />
+    <AirportDropdown />
+</template>
+
+<script setup>
+  import AirportHeader from '../components/AirportHeader.vue'
+  import AirportDropdown from '../components/AirportDropdown.vue'
+
+</script>
