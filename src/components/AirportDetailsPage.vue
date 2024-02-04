10aa0867800c4c3d19df5c645f8ce84cb19acfaa
Emalee Poellot <116819092+em2396@users.noreply.github.com>
2024-02-03 18:00:58 -0700

    Update AirportDetailsPage.vue

--- src/components/AirportDetailsPage.vue
+++ src/components/AirportDetailsPage.vue
@@ -1,20 +1,3 @@
 
-<template>
-  <div>
-    <h1>Hello {{ airportName }}</h1>
-  </div>
-</template>
-
-<script>
-export default {
-  props: {
-    airportName: {
-      type: String,
-      required: true
-    }
-  },
-};
-</script>
-
 
 
