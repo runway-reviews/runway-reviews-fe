8cee34ee58dcaba66a6a216e2a4e6f03ea4da2c7
Zen McMillan <zenmcmillan@gmail.com>
2024-02-07 18:07:55 -0600

    Test that when the submit button on the add review page is clicked the user is navigated back to the home page

--- cypress/e2e/form_spec.cy.js
+++ cypress/e2e/form_spec.cy.js
@@ -63,22 +63,40 @@ describe('Runway Reviews', () => {
     cy.get('input[class="review-input"]').should('exist')
     cy.get('button').contains('Submit')
   })
-  it('should take me back to the airport details page when x button is clicked', () => {
+  it('should take user back to the airport details page when x button is clicked', () => {
     cy.visit('http://127.0.0.1:5174/airport/Cancun%20International%20Airport')
 
-     cy.get('button').contains('Add Review').click()
-     cy.get('button').contains('✖️').click()
+    cy.get('button').contains('Add Review').click()
+    cy.get('button').contains('✖️').click()
 
-      cy.get('h1').contains('Cancun International Airport')
-      cy.get('button').contains('Add Review')
-      cy.get('button').contains('Home')
+    cy.get('h1').contains('Cancun International Airport')
+    cy.get('button').contains('Add Review')
+    cy.get('button').contains('Home')
 
-      cy.get('select')
+    cy.get('select')
     .get('option[value="Security"]').contains('Security')
     .get('option[value="Restaurants"]').contains('Restaurants')
     .get('option[value="General"]').contains("General")
     .get('option[value="Arrivals/Departures"]').contains("Arrivals/Departures")
     .get('option[value="Ammenities"]').contains("Ammenities")
     .get('option[value="Accessibility"]').contains("Accessibility")
+  })
+  it('should take user back to the home page after the submit button on the Add Review form is clicked', () => {
+
+    cy.visit('http://127.0.0.1:5174/airport/Cancun%20International%20Airport')
+
+    cy.get('button').contains('Add Review').click()
+
+    cy.get('button').contains('Submit').click()
+
+    cy.get('header').contains('Runway Reviews')
+    cy.get('button[class="login-button"]').contains("Login")
+
+    cy.get('select')
+    .get('option[value="John F. Kennedy International Airport"]').contains("John F. Kennedy International Airport")
+    .get('option[value="Denver International Airport"]').contains("Denver International Airport")
+    .get('option[value="Dallas Fort Worth International Airport"]').contains("Dallas Fort Worth International Airport")
+    .get('option[value="Cancun International Airport"]').contains("Cancun International Airport")
+    .get('option[value="Los Angeles International Airport"]').contains("Los Angeles International Airport")
   })
 })
