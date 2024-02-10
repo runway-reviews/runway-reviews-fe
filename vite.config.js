75466def3f83d28973cfdee78f70cdfa65808faf
Emalee Poellot <116819092+em2396@users.noreply.github.com>
2024-02-10 14:41:13 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+  }
   base: "runwawy-reviews/runway-reviews-fe",
   plugins: [
     vue(),
