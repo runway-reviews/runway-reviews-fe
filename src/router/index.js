5a67404d77229b3a3c89b505ef1ff0d69d3ca8be
Emalee Poellot <emalee.poellot@gmail.com>
2024-03-14 13:27:09 -0600

    feat/Add semicolon

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 // import { createStore } from 'vuex'
-import AirportDetailsPage from '../views/AirportDetailsPage.vue'
-import Home from '../views/Home.vue'
-import Login from '../components/Login.vue'
-import AddReview from '../views/AddReview.vue'
-import CreateAccount from '../views/CreateAccount.vue'
+import AirportDetailsPage from '../views/AirportDetailsPage.vue';
+import Home from '../views/Home.vue';
+import Login from '../components/Login.vue';
+import AddReview from '../views/AddReview.vue';
+import CreateAccount from '../views/CreateAccount.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
